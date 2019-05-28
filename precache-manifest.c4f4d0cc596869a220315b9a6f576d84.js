dc4f87f295a977dd8b5c2601269d4317d74d0fae
akluong <aluong112296@gmail.com>
2019-05-28 14:50:09 -0700

    Updates

---
+++ precache-manifest.c4f4d0cc596869a220315b9a6f576d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb4237bc160e5d584452815cd7283a3",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "9545fa0bde4649edc1d9",
+    "url": "/pokedex/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "bd383a59dab1001e052d",
+    "url": "/pokedex/static/css/main.79f961e9.chunk.css"
+  },
+  {
+    "revision": "9545fa0bde4649edc1d9",
+    "url": "/pokedex/static/js/2.8f559f78.chunk.js"
+  },
+  {
+    "revision": "bd383a59dab1001e052d",
+    "url": "/pokedex/static/js/main.d91c4c60.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
